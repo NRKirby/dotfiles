196187ce8ff53c7c60603400ac5d9feca1349509
Nick Kirby <nick.kirby@ao.com>
2022-05-31 10:09:06 +0100

    Add profile script

---
+++ windows/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,4 @@
+# Store this after running:
+# notepad $profile
+Set-Alias -Name t -Value terraform
+Set-Alias -Name rider -Value "C:\Users\Nick\AppData\Local\JetBrains\Installations\Rider213\bin\rider64.exe"
