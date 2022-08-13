73d9b1e6d094e8cf8989ccaad4b64a57453be821
Nick Kirby <nrkirb@gmail.com>
2022-08-13 14:19:13 +0100

    Create config.sh

---
+++ mac/config.sh
@@ -0,0 +1,16 @@
+# Remove dock delay
+defaults write com.apple.dock autohide-time-modifier -int 0;killall Dock
+
+# Productivity tools (de-macify)
+
+# AltTab
+brew install --cask alt-tab
+
+# Itsycal
+brew install --cask itsycal
+
+# Rectangle
+brew install --cask rectangle
+
+# Shottr
+brew install --cask shottr
