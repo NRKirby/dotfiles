db6fc2ab3176ee7dc830eec5afec8a8db3e5c58d
Nick Kirby <nrkirb@gmail.com>
2023-04-24 08:18:05 +0100

    Update setup.sh

--- mac/setup.sh
+++ mac/setup.sh
@@ -1,6 +1,9 @@
 # Remove dock delay
 defaults write com.apple.dock autohide-time-modifier -int 0;killall Dock
 
+# Increase mouse tracking speed
+defaults write -g com.apple.mouse.scaling -float 12
+
 # Tools for Mac
 brew install --cask alt-tab
 
