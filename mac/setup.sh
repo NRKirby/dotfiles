7e0e71fb16ee968bcb3f073efba44ee24c703b95
Nick Kirby <nrkirb@gmail.com>
2023-05-26 21:28:21 +0100

    Update setup.sh

--- mac/setup.sh
+++ mac/setup.sh
@@ -4,6 +4,9 @@ defaults write com.apple.dock autohide-time-modifier -int 0;killall Dock
 # Increase mouse tracking speed
 defaults write -g com.apple.mouse.scaling -float 12
 
+# Always show hidden files in Finder
+defaults write com.apple.finder AppleShowAllFiles YES
+
 # Tools for Mac
 brew install --cask alt-tab
 
